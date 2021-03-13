eb496718fc05a909493e5840c4ca808c1dd6848a
Joao Correia <joaopedro.correia@decision.tech>
2021-03-13 18:26:58 +0000

    add missing photo property to user

--- src/Asana/Models/User.cs
+++ src/Asana/Models/User.cs
@@ -5,13 +5,16 @@ namespace Asana.Models
     public sealed class User : AsanaNamedResource
     {
         public string Email { get; }
-        public string Photo { get; }
+        public UserPhoto Photo { get; }
+        public Workspace[] Workspaces { get; }
 
         [JsonConstructor]
-        internal User(string gid, string resourceType, string name, string email, string photo) : base(gid, resourceType, name)
+        internal User(string gid, string resourceType, string name, string email, UserPhoto photo, Workspace[] workspaces) : base(gid, resourceType, name)
         {
             Email = email;
             Photo = photo;
+            Workspaces = workspaces;
         }
+
     }
 }
---
+++ src/Asana/Models/UserPhoto.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Asana.Models
+{
+    public sealed class UserPhoto
+    {
+        [JsonProperty("image_128x128")]
+        public string Image128x128 { get; }
+        [JsonProperty("image_21x21")]
+        public string Image21x21 { get; }
+        [JsonProperty("image_27x27")]
+        public string Image27x27 { get; }
+        [JsonProperty("image_36z36")]
+        public string Image36x36 { get; }
+        [JsonProperty("image_60x60")]
+        public string Image60x60 { get; }
+
+        [JsonConstructor]
+        public UserPhoto(string image128X128, string image21X21, string image27X27, string image36X36, string image60X60)
+        {
+            Image128x128 = image128X128;
+            Image21x21 = image21X21;
+            Image27x27 = image27X27;
+            Image36x36 = image36X36;
+            Image60x60 = image60X60;
+        }
+    }
+}
